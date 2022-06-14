2793941945149e744e3483ddc5e960997f71ec85
zohaib166 <zohaib166@gmail.com>
2022-06-14 20:26:45 +0530

    menu using media

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents'
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={ this.state.dishes }/>
       </div>
     );
   }
