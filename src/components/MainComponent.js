9e136bf8a3afaa4e86b3132458ba989b5da5dbc3
zohaib166 <zohaib166@gmail.com>
2022-07-10 00:19:08 +0530

    Page Transition Animation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -17,6 +17,7 @@ import {
   fetchPromos,
 } from "../redux/ActionCreators";
 import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const mapStateToProps = (state) => {
   return {
@@ -91,25 +92,29 @@ class Main extends Component {
       <div>
         <div className="container">
           <Header />
-          <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route exact path="/aboutus" component={AboutUsPage} />
-            <Route
-              exact
-              path="/menu"
-              component={() => <Menu dishes={this.props.dishes} />}
-            />
-            <Route path="/menu/:dishId" component={DishWithId} />
-            <Route
-              exact
-              path="/contactus"
-              component={() => (
-                <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
-              )}
-            />
+          <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+            <Switch>
+              <Route path="/home" component={HomePage} />
+              <Route exact path="/aboutus" component={AboutUsPage} />
+              <Route
+                exact
+                path="/menu"
+                component={() => <Menu dishes={this.props.dishes} />}
+              />
+              <Route path="/menu/:dishId" component={DishWithId} />
+              <Route
+                exact
+                path="/contactus"
+                component={() => (
+                  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                )}
+              />
 
-            <Redirect to="/home" />
-          </Switch>
+              <Redirect to="/home" />
+            </Switch>
+            </CSSTransition>
+          </TransitionGroup>
           <Footer />
         </div>
       </div>
